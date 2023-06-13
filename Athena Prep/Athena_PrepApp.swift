8de82575b4d7bfc3a785eeec28a9ae15397b3003
Avanti Singh <avantisingh19@gmail.com>
2023-06-13 09:25:01 -0500

    Initial Commit

---
+++ Athena Prep/Athena_PrepApp.swift
@@ -0,0 +1,20 @@
+//
+//  Athena_PrepApp.swift
+//  Athena Prep
+//
+//  Created by scholar on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Athena_PrepApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
